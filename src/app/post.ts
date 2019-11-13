1db5d1ceda3c393076905dc1b9ad7068662f8138
Evolve <Artevolve@yahoo.com>
2019-11-13 03:24:10 +0100

    Created Post Class

---
+++ src/app/post.ts
@@ -0,0 +1,21 @@
+import * as moment from "moment";
+
+class Post {
+  title: string;
+  content: string;
+  loveIts: number;
+
+  private readonly created_at: moment.Moment;
+  private readonly dateFormat = "MM/DD/YYYY, HH:mm A";
+
+  constructor(title: string, content: string, loveIts: number) {
+    this.title = title;
+    this.content = content;
+    this.loveIts = loveIts;
+    this.created_at = moment();
+  }
+
+  public get creationDate(): string {
+    return moment(this.created_at).format(this.dateFormat);
+  }
+}
